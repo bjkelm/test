55a8a70d0c9ec8aab26b62676174e4db17b08045
Luka <Luka.Lee@raon-tech.com>
2022-02-21 13:16:58 +0900

    Update classs.java

--- src/tesst1/classs.java
+++ src/tesst1/classs.java
@@ -1,37 +1,37 @@
 package tesst1;
 
-class tv 
-{
-	String color;
-	int channel;
-	boolean power;
-	
-	void power() 
-	{
-		power = !power;
-	}
-	void up()
-	{
-		channel++;
-	}
-	void down()
-	{
-		channel--;
-	}
-}
 
 public class classs {
 	public static void main(String[] args)
 	{
-		tv t = new tv();
-		
-		t.power = true;
-		t.channel = 8;
-		t.color = "빨강";
-		
-		t.down();
-		
-		System.out.printf("현재 채널은 %s이고 전원은 %b이며 채널은  %d입니다.",t.color, t.power, t.channel);
-	}
+		time cur = new time();
+}
 
+class time
+{
+	public int hour;
+	private int minute;
+	private float second;
+
+	public int gethour() {return hour;}
+	public int getmin() {return minute;}
+	public float getsec() {return second;}
+	
+	public void sethour(int h)
+	{
+		if (h<0 || h>23 ) return;
+		hour = h;
+	}
+	
+	public void setminute(int m)
+	{
+		if(m<0 || m>60) return;
+		minute = m;
+	}
+	
+	public void setsecond(float s)
+	{
+		if( s< 0.0f || s>59.99f) return;
+		second = s;
+	}
 }
