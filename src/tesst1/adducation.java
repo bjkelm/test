5c29668b33173ac63ef9f6fb3a7feb1d07a9a3a1
Luka <Luka.Lee@raon-tech.com>
2022-02-21 10:47:06 +0900

    Update adducation.java
    이거는 어... 지뢰찾기? 몰

--- src/tesst1/adducation.java
+++ src/tesst1/adducation.java
@@ -5,19 +5,62 @@ import java.util.*;
 public class adducation {
 	public static void main(String[] args)
 	{
-		String name[] = {"Luka", "Lee"};
+		final int size =10;
+		int x=0, y=0;
 		
-		for(int i=0; i <name.length;i++)
+		char[][] board = new char[size][size];
+		
+		for(int i=0; i<size ; i++)
+			for(int j=0; j<size; j++)
+				board[i][j]= ' ';
+		
+		byte[][] shipboard = {
+		//1 2  3  4  5  6  7  8  9
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{1, 1, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{1, 1, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{1, 1, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{1, 1, 0, 0, 0, 0, 0, 0, 0},
+		{1, 1, 0, 0, 0, 0, 0, 0, 0},
+		};
+		
+		for(int i=1; i<size; i++)
 		{
-			System.out.println(i + "번째는" + name[i] );
+			board[0][i] = board[i][0] = (char)(i+'0');
 		}
-		String tmp = name[1];
-		System.out.println(tmp);
-		name[0] = "Daniel";
+		Scanner scanner = new Scanner(System.in);
 		
-		for(String str : name) {
-			System.out.println(str);
-		}
+		while(true)
+		{
+			System.out.printf("좌표를 입력하세요. (종료는 00)>");
+			String input = scanner.nextLine();
+			
+			if(input.length() == 2)
+			{
+				x= input.charAt(0) - '0';
+				y=input.charAt(1) - '0';
+				
+				if(x==0 && y==0)
+					break;
+			}
+			
+			if(input.length()!=2 || x<=0 || x>=size || y<=0 || y>=size)
+			{
+				System.out.println("잘못된 입력입니다. 다시 입력해주세요");
+				continue;
+			}
+			
+			board[x][y]= shipboard[x-1][y-1] == 1 ? 'O' : 'X';
+			
+			for(int i=0; i<size; i++)
+			{
+				System.out.println(board[i]);
+			}
+			System.out.println();
+			}
 	}
 
 }
